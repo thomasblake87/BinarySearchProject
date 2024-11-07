2891dc16636d30b4e1393f807f6cce6491b75f76
Thomas Blake <tb69613@email.vccs.edu>
2024-11-06 23:08:58 -0500

    Updates

--- BinarySearchProject.cpp
+++ BinarySearchProject.cpp
@@ -3,136 +3,103 @@
 #include <algorithm>
 #include <random>
 #include <ctime>
+#include <chrono>
 
 using namespace std;
 
-void printVector(const vector<int>& vec) {
-	for (int item : vec) {
-		cout << item << " ";
-
-	}
-	cout << endl;
-}
-
-//Recursive binary search
-
-int recursiveBinarySearch(const vector<int>& vec, int target, int low, int high) {
-	if (low <= high) {
-		int mid = low + (high - low) / 2;
-
-		if (vec[mid] == target) {
-			return mid; //Target found
-		}
-		else if (vec[mid] < target) {
-			return recursiveBinarySearch(vec, target, mid + 1, high);
-		}
-		else {
-			return recursiveBinarySearch(vec, target, low, mid - 1);
-		}
-	}
-	else {
-		return -1;
-	}
-}
-
-int iterativeBinarySearch(const std::vector<int>& vec, int target) {
-	int low = 0, high = vec.size() - 1;
-	while (low <= high) {
-		int mid = low + (high - low) / 2;
-		if (vec[mid] == target) {
-			return mid;
-		}
-		else if (vec[mid] > target) {
-			high = mid - 1;
-		}
-		else {
-			low = mid + 1;
-		}
-	}
-	return -1; // Target not found
-}
-
-// Sequential Search Function
-int sequentialSearch(const std::vector<int>& vec, int target) {
-	for (int i = 0; i < vec.size(); ++i) {
-		if (vec[i] == target) {
-			return i;
-		}
-	}
-	return -1; // Target not found
-}
+int recursiveBinarySearch(const vector<int>& vec, int target, int low, int high);
+int iterativeBinarySearch(const std::vector<int>& vec, int target);
+int sequentialSearch(const std::vector<int>& vec, int target);
 
 int main() {
-	srand(time(0));
+    // Variables
+    int n = 1000000;
+    double sumRBS = 0;
+    double sumIBS = 0;
+    double sumSeqS = 0;
 
-	int x = rand() % 100 + 1;
-	int y = rand() % 100 + 1;
-	int z = rand() % 100 + 1;
-	int a = rand() % 100 + 1;
-	int b = rand() % 100 + 1;
-	int c = rand() % 100 + 1;
-	vector<int> data = { a, b, c, x, y, z }; //Numbers to test
+    srand(time(0));
 
-	//Sort vector data
-	sort(data.begin(), data.end());
+    for (int i = 0; i < 10; ++i) {
+        vector<int> data(n);
 
-	//Print sorted vector (debugging)
-	cout << "Sorted vector: " << endl;
-	printVector(data);
+        for (int& num : data) {
+            num = rand() % 100 + 1;
+        }
 
-	//target1 is in the list, target2 is not
+        // Sort vector data
+        sort(data.begin(), data.end());
 
-	int target1 = rand() % 6;
-	if (target1 == 0) {
-		target1 = x;
-	}
-	else if (target1 == 1) {
-		target1 = y;
-	}
-	else if (target1 == 2) {
-		target1 = z;
-	}
-	else if (target1 == 3) {
-		target1 = a;
-	}
-	else if (target1 == 4) {
-		target1 = b;
-	}
-	else if (target1 == 5) {
-		target1 = c;
-	}
+        int target1 = rand() % 100 + 1;
 
-	int recursiveBinaryResult = recursiveBinarySearch(data, target1, 0, data.size() - 1);
-	//Recursive binary search
-	if (recursiveBinaryResult != -1) {
-		std::cout << "Target " << target1 << " found at location " << recursiveBinaryResult << endl;
-	}
-	else {
-		std::cout << "Target " << target1 << " was not found" << endl;
-	}
+        // Recursive binary search timing
+        auto startRBS = std::chrono::high_resolution_clock::now();
+        recursiveBinarySearch(data, target1, 0, data.size() - 1);
+        auto endRBS = std::chrono::high_resolution_clock::now();
+        sumRBS += std::chrono::duration_cast<std::chrono::microseconds>(endRBS - startRBS).count();
 
+        // Iterative binary search timing
+        auto startIBS = std::chrono::high_resolution_clock::now();
+        iterativeBinarySearch(data, target1);
+        auto endIBS = std::chrono::high_resolution_clock::now();
+        sumIBS += std::chrono::duration_cast<std::chrono::microseconds>(endIBS - startIBS).count();
 
-	//Iterative binary search
-	int IBR = iterativeBinarySearch(data, target1);
+        // Iterative sequential search timing
+        auto startseqS = std::chrono::high_resolution_clock::now();
+        sequentialSearch(data, target1);
+        auto endseqS = std::chrono::high_resolution_clock::now();
+        sumSeqS += std::chrono::duration_cast<std::chrono::microseconds>(endseqS - startseqS).count();
+    }
 
-	cout << "Using iterative binary search: " << endl;
-	if (IBR != -1) {
-		std::cout << "Target " << target1 << " found at location " << IBR << endl;
-	}
-	else {
-		std::cout << "Target " << target1 << " was not found" << endl;
-	}
+    cout << "Average running time for recursive binary search in microseconds is: " << sumRBS / 10 << endl;
+    cout << "Average running time for iterative binary search in microseconds is: " << sumIBS / 10 << endl;
+    cout << "Average running time for sequential search in microseconds is: " << sumSeqS / 10 << endl;
 
-	//Sequential search
-	int SS = sequentialSearch(data, target1);
+    return 0;  // Added return statement for main
+}
 
-	cout << "Using sequential search: " << endl;
-	if (SS != -1) {
-		std::cout << "Target " << target1 << " found at location " << SS << endl;
-	}
-	else {
-		std::cout << "Target " << target1 << " was not found" << endl;
-	}
+// Recursive binary search
+int recursiveBinarySearch(const vector<int>& vec, int target, int low, int high) {
+    if (low <= high) {
+        int mid = low + (high - low) / 2;
 
-	return 0;
+        if (vec[mid] == target) {
+            return mid; // Target found
+        }
+        else if (vec[mid] < target) {
+            return recursiveBinarySearch(vec, target, mid + 1, high);
+        }
+        else {
+            return recursiveBinarySearch(vec, target, low, mid - 1);
+        }
+    }
+    return -1;
+}
+
+// Iterative binary search
+int iterativeBinarySearch(const std::vector<int>& vec, int target) {
+    int low = 0, high = vec.size() - 1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (vec[mid] == target) {
+            return mid;
+        }
+        else if (vec[mid] > target) {
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+    return -1; // Target not found
+}
+
+// Sequential search function
+int sequentialSearch(const std::vector<int>& vec, int target) {
+    for (int i = 0; i < vec.size(); ++i) {
+        if (vec[i] == target) {
+            return i;
+        }
+    }
+    return -1; // Target not found
 }
